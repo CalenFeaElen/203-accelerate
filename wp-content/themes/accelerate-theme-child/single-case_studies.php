eb0139287aba80efeb83ea614f1c75187b1528a6
CalenFeaElen <CalenFeaElen@gmail.com>
2020-08-27 15:26:32 +1000

    Edited case study page custom fields.

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,12 +14,48 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
+	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+			<?php while ( have_posts() ) : the_post(); 
+				$size = "full";
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3'); ?>
+		<article class="case-study">
+			<aside class="case-study-sidebar">
+				<div class="case-study-descriptions">
+					<h2><?php the_title(); ?></h2>
+					<h5><?php echo $services; ?></h5>
+					<h6><?php echo $client; ?></h6>
+
+					<?php the_content(); ?>
+
+					<p><strong><a href="<?php echo $link; ?>">Visit Live Site</a></strong></p>
+				</div>
+
+				<div class="case-study-images">
+
+					<?php if($image_1) { 
+						echo wp_get_attachment_image( $image_1, $size );
+					} ?>
+					<?php if($image_2) { 
+						echo wp_get_attachment_image( $image_2, $size );
+					} ?>
+					<?php if($image_3) { 
+						echo wp_get_attachment_image( $image_3, $size );
+					} ?>
+
+				</div>
+			</aside>
+		</article>
+				
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
-
+<nav id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr; <span>Back to Work</span></a></div>
+</nav>
 <?php get_footer(); ?>
